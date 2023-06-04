dcc2cfed029bcf507f8a6be9e6cb052bce083e04
mindthegeekgr <92975599+mindthegeekgr@users.noreply.github.com>
2023-06-04 19:51:40 +0300

    function conversions
    Den douleve sosta gia to K

--- final_project_390.R
+++ final_project_390.R
@@ -75,3 +75,65 @@ e1
 
 D = (k-w) / sqrt((e1*s) + (e2*s*(s-1)))
 D
+
+# den doulevei meta to 3251 DEN DOULEVEI
+calculate_k <- function(input_vector){
+  diff_count = vector("numeric",length = 50)
+  
+  i=1
+  while (i != 50) {
+    
+    for (j in (i+1):50) {
+      temp <- sum(input_vector[[i]] != input_vector[[j]])
+      diff_count <- append(diff_count,temp)  
+    }
+    i = i+1
+    
+  }
+  #Υπολογισμος k
+  
+  k = sum(diff_count/50)
+  k
+  
+}
+
+#function υπολογισμου w
+calculate_w <- function(s){
+  a1 = sum(1/(1:49))
+  
+  w = s/a1
+  w
+}
+
+
+#Function υπολογισμου D
+calculate_D <- function(k,w,s){
+  n=50
+  a1 = sum(1/(1:49))
+  a2 = sum(1/((1:49)**2))
+  a2
+  
+  b2 = (2*(n**2+n+3))/(9*n*(n-1))
+  b2
+  
+  b1 = (n+1)/(3*(n-1))
+  b1
+  
+  c2 = b2 - (n+2)/(a1*n) + a2/(a1**2)
+  c2
+  
+  c1 = b1 - 1/a1
+  c1
+  
+  e2 = c2/((a1**2)+a2)
+  e2
+  
+  e1 = c1/a1
+  e1
+  
+  D = (k-w) / sqrt((e1*s) + (e2*s*(s-1)))
+  D
+}
+
+calculate_w(132)
+calculate_D(349.92,29.46951,132)
